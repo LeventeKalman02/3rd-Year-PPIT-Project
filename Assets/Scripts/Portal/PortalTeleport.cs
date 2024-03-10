f90c0be966550b952ec5fafbd7dff7c54924c913
EnhancedTarget <levente.kalman922@gmail.com>
2024-03-10 22:59:25 +0000

    Portal, portal animation

---
+++ Assets/Scripts/Portal/PortalTeleport.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalTeleport : MonoBehaviour
+{
+    //when player enters the portal, transfer to the fighting arena
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
