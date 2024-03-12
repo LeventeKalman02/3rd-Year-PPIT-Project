d45f4164e622ab633a3254423db262c6a41c8d3c
EnhancedTarget <levente.kalman922@gmail.com>
2024-03-12 13:23:55 +0000

    comments

--- Assets/Scripts/MainMenuScripts/BackButton.cs
+++ Assets/Scripts/MainMenuScripts/BackButton.cs
@@ -6,6 +6,8 @@ public class BackButton : MonoBehaviour
 {
     private SceneManagerScript setPreviousScene;
 
+    //gets the object with the tag "scenemanager" and get the script component
+    //calls the BackButton function from the scene manager script
     public void Back()
     {
         setPreviousScene = GameObject.FindWithTag("SceneManager").GetComponent<SceneManagerScript>();
